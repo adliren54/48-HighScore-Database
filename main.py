6e4bae1c2f3c2452895bc9ffae9826e342d759d8
adliren54 <62419296+adliren54@users.noreply.github.com>
2023-05-04 20:29:25 +0000

    add the loop

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 f = open("high.score", "a+")
-initial = input("INITIALS > ")
-f.write(f"{initial}\n")
-score = input("SCORE > ")
-f.write(f"{score}\n")
+i = 0
+while True:
+  i += 1
+  initial = input("INITIALS > ")
+  f.write(f"{initial}\n")
+  score = input("SCORE > ")
+  f.write(f"{score}\n")
+  if i >= 3:
+    proceed = input("Have you finished entering the data?\n>").lower()
+    if proceed[0] == "n":
+      continue
+    elif proceed [0] == "y":
+      break
 f.close()
